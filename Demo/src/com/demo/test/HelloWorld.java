5ac4315319d2a0c51c81e1bb5bd16bf69e3f8676
wangbin <wangbin@gmail.com>
2011-07-22 12:56:53 +0800

    版本1.0

---
+++ Demo/src/com/demo/test/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.demo.test;
+
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		// version 1.0
+		System.out.println("hello,version 1.0");
+	}
+
+}
